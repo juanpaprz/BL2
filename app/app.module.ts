f0f3dc3b933eeab3167971eeee0531a669cfb533
juanpaprz <54371833+juanpaprz@users.noreply.github.com>
2023-05-15 15:34:28 -0600

    2 files modified, 3 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ApiManagementComponent } from './Components/api-management/api-management.component';
+
+const appRoutes: Routes = [{ path: 'api', component: ApiManagementComponent }];
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  declarations: [AppComponent, ApiManagementComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
