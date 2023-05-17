93ffecd454cc3db3692795777108518d79aff196
juanpaprz <54371833+juanpaprz@users.noreply.github.com>
2023-05-17 13:49:03 -0600

    custom validation

--- app/Components/api-body/api-body.component.ts
+++ app/Components/api-body/api-body.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AtLeastOneCheckbox,
+  ValidationService,
+} from '../../Services/validation.service';
 import { BodyCode } from '../../Entities/BodyCode';
 import { ApiFirebaseService } from '../../Services/api-firebase.service';
+import { TypeCode } from '../../Entities/TypeCode';
 
 @Component({
   selector: 'app-api-body',
@@ -12,10 +17,26 @@ export class ApiBodyComponent implements OnInit {
   form: FormGroup = new FormGroup([]);
 
   bodyCodes: BodyCode[] = [];
+  typeCodes: TypeCode[] = [];
 
-  constructor(private dbService: ApiFirebaseService) {}
+  constructor(
+    private dbService: ApiFirebaseService,
+    private validateService: ValidationService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      bodyId: new FormControl('', Validators.required),
+      types: new FormArray([], AtLeastOneCheckbox()),
+    });
+
+    this.getBodyCodes();
+    this.getTypeCodes();
+  }
+
+  get types() {
+    return this.form.get('types') as FormArray;
+  }
 
   getBodyCodes() {
     this.dbService.getAllBodyCodes().subscribe({
@@ -23,5 +44,35 @@ export class ApiBodyComponent implements OnInit {
         if (response) this.bodyCodes = Object.values(response);
       },
     });
+  }
+
+  getTypeCodes() {
+    this.dbService.getAllTypeCodes().subscribe({
+      next: (response) => {
+        if (response) this.typeCodes = Object.values(response);
+
+        if (this.typeCodes.length) {
+          this.typeCodes.forEach((typeCode) => {
+            this.types.push(new FormControl(false));
+          });
+        }
+      },
+    });
+  }
+
+  isFieldValid(field: string): boolean {
+    return this.validateService.isFieldValid(this.form, field);
+  }
+
+  setInvalidClass(field: string) {
+    return this.validateService.setInvalidClass(this.form, field);
+  }
+
+  addBody() {
+    if (this.form.valid) {
+    } else {
+      console.log(this.form);
+      this.validateService.toucheFields(this.form, true);
+    }
   }
 }
