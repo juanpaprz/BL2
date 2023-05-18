e5b4e07f2780737f29a53ffa623a346197946665
juanpaprz <54371833+juanpaprz@users.noreply.github.com>
2023-05-18 11:16:29 -0600

    pagination continue

--- app/Components/Common/table-pagination/table-pagination.component.ts
+++ app/Components/Common/table-pagination/table-pagination.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-pagination',
   templateUrl: './table-pagination.component.html',
-  styleUrls: ['./table-pagination.component.css']
+  styleUrls: ['./table-pagination.component.css'],
 })
-export class TablePaginationComponent implements OnInit {
+export class TablePaginationComponent implements OnInit, OnChanges {
+  @Input() totalData: number = 0;
 
-  constructor() { }
+  @Input() displayValue: number = 0;
 
-  ngOnInit() {
+  constructor() {}
+
+  pages: number[] = [];
+  pagesDisplay: number[] = [];
+
+  currentPage: number = 0;
+
+  ngOnInit() {}
+
+  ngOnChanges() {
+    if (this.totalData > 0) {
+      if (this.displayValue > this.totalData)
+        this.displayValue = this.totalData;
+
+      this.getPagesDisplay();
+    }
   }
 
+  getPages() {
+    let total = Math.ceil(this.totalData / this.displayValue);
+
+    for (let i = 0; i < total; i++) {
+      this.pages.push(i + 1);
+    }
+  }
+
+  getPagesDisplay() {
+    this.getPages();
+
+    if (this.pages.length <= 3) {
+      this.pagesDisplay = this.pages;
+      return;
+    }
+
+    this.pagesDisplay = this.pages.slice(this.currentPage, 3);
+    console.log(this.pagesDisplay);
+  }
 }
